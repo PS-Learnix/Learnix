0ae356bdb529e17863abba8f45cc5b4c51ab1125
YoshuaPariona <loganpariona.1207@gmail.com>
2026-05-24 18:30:37 +0000

    feat: Redirigir root al login

--- web-app/src/main/java/com/learnix/web/HomeController.java
+++ web-app/src/main/java/com/learnix/web/HomeController.java
@@ -1,31 +1,13 @@
 package com.learnix.web;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/")
+@Controller
 public class HomeController {
 
-    @Value("${spring.application.name}")
-    private String appName;
-
-    @GetMapping
-    public ResponseEntity<Map<String, Object>> getIndex() {
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("name", appName);
-        response.put("version", "0.0.1");
-        response.put("timestamp", LocalDateTime.now());
-        response.put("endpoint_docs", "/swagger-ui/index.html");
-
-        return ResponseEntity.ok().body(response);
+    @GetMapping("/")
+    public String redirectLogin() {
+        return "redirect:/login";
     }
 }
